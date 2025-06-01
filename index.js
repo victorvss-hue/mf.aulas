187e5eb2ab705a73749b24f6117141775a27d5f9
mf.aulas <ve537698@gmail.com>
2025-06-01 19:20:02 -0300

    commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//Declare uma variável para armazenar o nome de um aluno, sua idade, e 
+//sua nota final. Exiba valores no console.
+
+let nome = "Fábio Oliveira"
+let idade = 18
+let notaFinal = 9.8
+
+console.log("nome: ", nome, "Idade: ", idade, "Nota Final: ", notaFinal)
